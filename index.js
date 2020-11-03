f1fc230e4381ee1b6eb9ccd8c5b3b6c152b68ffb
NTTien-PTC <truongtienmc1999@gmail.com>
2020-11-03 11:33:58 +0700

    23

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use(fileUpload())
 
 //ket noi mongodb tu node.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
 const uri = "mongodb+srv://admin:admin@123@cluster0.hl5f0.mongodb.net/<dbname>?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true })
-
+// mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 // const MongoClient = require('mongodb').MongoClient;
 
 // const client = new MongoClient(uri, { useNewUrlParser: true });
